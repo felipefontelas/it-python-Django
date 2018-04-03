4ea3398e685367a4cf135066425781c84acbb573
felipefontelas <felipefontelas.123@gmail.com>
2018-04-03 12:25:09 -0300

    Backend iniciado

--- iTimetabling/views.py
+++ iTimetabling/views.py
@@ -2,8 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Instituicao
 
 # Create your views here.
 
 def instituicao_list(request):
-    return render(request, 'iTimetabling/instituicao_list.html', {})
+    instituicoes = Instituicao.objects.all().order_by('created_at')
+    return render(request, 'iTimetabling/instituicao_list.html', {'instituicoes': instituicoes})
