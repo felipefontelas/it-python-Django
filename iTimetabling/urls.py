23096d0aa4083fb9b319f6d9cf6e970d880b9536
felipefontelas <felipefontelas.123@gmail.com>
2018-04-04 09:08:32 -0300

    criacao detail

--- iTimetabling/urls.py
+++ iTimetabling/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.instituicao_list, name='instituicao_list')
+    url(r'^$', views.instituicao_list, name='instituicao_list'),
+    url(r'^instituicao/(?P<pk>[0-9]+)/$', views.instituicao_detail, name='instituicao_detail'),
 ]
--- iTimetabling/views.py
+++ iTimetabling/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Instituicao
 
@@ -10,3 +10,7 @@ from .models import Instituicao
 def instituicao_list(request):
     instituicoes = Instituicao.objects.all().order_by('created_at')
     return render(request, 'iTimetabling/instituicao_list.html', {'instituicoes': instituicoes})
+
+def instituicao_detail(request, pk):
+    instituicao = get_object_or_404(Instituicao, pk=pk)
+    return render(request, 'iTimetabling/instituicao_detail.html', {'instituicao': instituicao})
